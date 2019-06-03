ef7b92f6d7eb6423fb57c13ac398f18a14bc9981
Ubuntu <ubuntu@dummy-task.us-central1-a.c.kubernetes-project-239207.internal>
2019-06-03 10:35:26 +0000

    Routes changed

--- routes/users.js
+++ routes/users.js
@@ -11,80 +11,80 @@ const validator = require('../validator/Validator');
 //enroll admin
 router.get('/enroll-admin', function (req, res, next) {
     CAClientController.enrollAdmin(req.body).then(result => {
-        res.status(200).send(result);
+        res.status(result.status).json(result.data);
     }).catch(err => {
         //error handling
-        res.status(err.httpStatus || 500).send({ message: err.message, status: 0 });
+        res.status(result.status).json(result.data);
     });
 });
 
 //enroll user1
 router.get('/enroll-user', function (req, res, next) {
     CAClientController.enrollUser(req.body).then(result => {
-        res.status(200).send(result);
+        res.status(result.status).json(result.data);
     }).catch(err => {
         //error handling
-        res.status(err.httpStatus || 500).send({ message: err.message, status: 0 });
+        res.status(result.status).json(result.data);
     });
 });
 
 //create a new user
 router.post('/create-user', validator.createUser, function (req, res, next) {
     user_object.createUser(req.body).then(result => {
-        res.status(200).send(result);
+        res.status(result.status).json(result.data);
     }).catch(err => {
         //error handling
-        res.status(err.httpStatus || 500).send({ message: err.message, status: 0 });
+        res.status(result.status).json(result.data);
     });
 });
 
 //get user details
 router.post('/get-user', validator.getUser, function (req, res, next) {
     user_object.getUser(req.body).then(result => {
-        res.status(200).send(result);
+        res.status(result.status).json(result.data);
     }).catch(err => {
         //error handling
-        res.status(err.httpStatus || 500).send({ message: err.message, status: 0 });
+        res.status(result.status).json(result.data);
     });
 });
 
 //get all users
 router.post('/all-users', validator.allUsers, function (req, res, next) {
     user_object.allUsers(req.body).then(result => {
-        res.status(200).send(result);
+        res.status(result.status).json(result.data);
     }).catch(err => {
         //error handling
-        res.status(err.httpStatus || 500).send({ message: err.message, status: 0 });
+        res.status(result.status).json(result.data);
     });
 });
 
 //issue token
 router.post('/issue-token', validator.issueToken, function (req, res, next) {
     user_object.issueToken(req.body).then(result => {
-        res.status(200).send(result);
+        res.status(result.status).json(result.data);
     }).catch(err => {
         //error handling
-        res.status(err.httpStatus || 500).send({ message: err.message, status: 0 });
+        res.status(result.status).json(result.data);
     });
 });
 
 //get tokens
 router.post('/get-token', validator.getToken, function (req, res, next) {
     user_object.getToken(req.body).then(result => {
-        res.status(200).send(result);
+        res.status(result.status).json(result.data);
     }).catch(err => {
         //error handling
-        res.status(err.httpStatus || 500).send({ message: err.message, status: 0 });
+        res.status(result.status).json(result.data);
     });
 });
 
 //transfer token
 router.post('/transfer-token', validator.transferToken, function (req, res, next) {
     user_object.transferToken(req.body).then(result => {
-        res.status(200).send(result);
+        res.status(result.status).json(result.data);
     }).catch(err => {
         //error handling
-        res.status(err.httpStatus || 500).send({ message: err.message, status: 0 });
+        res.status(result.status).json(result.data);
     });
 });
 
